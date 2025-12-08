17c6236da9da2d57ce23ef617356eda2f4094370
mazhar <mazharopal@gmail.com>
2025-12-08 14:44:24 +0500

    changes in customer and navbar

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://authsandtasks.pythonanywhere.com",
+    "anmoltailor.pythonanywhere.com",
    
 ]
 
