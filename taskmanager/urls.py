c50bf84b86567e7a6f65afe0c4291dd3aba3846a
mazhar <mazharopal@gmail.com>
2025-12-10 09:59:21 +0500

    changes in url.py

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     # React frontend
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!admin).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 
